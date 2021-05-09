daa16f626fb5d09d6f7c4b008f7f34917b71756a
uysalserkan <uysalserkan08@gmail.com>
2021-05-09 15:01:19 +0300

    Ödeve göre uygun olarak tekrar düzenlendi.

--- Operating Systems/os_odev.c
+++ Operating Systems/os_odev.c
@@ -9,69 +9,9 @@ int *arr;
 int counter = 0;
 sem_t mutex;
 
-void *thread_1_adding(void *val)
-{
-  int SIZE;
-  SIZE = *((int *)val);
-
-  while (1)
-  {
-    sem_wait(&mutex);
-
-    if (counter != SIZE)
-    {
-      total = total + arr[counter];
-
-      printf("Thread1 %d değerini okudu toplam %d \n", arr[counter], total);
-
-      counter++;
-    }
-    else
-    {
-      printf("Thread1 okunacak değer bulunamadı.\n");
-
-      sem_post(&mutex);
-
-      break;
-    }
-
-    sem_post(&mutex);
-  }
-  return NULL;
-}
-
-void *thread_2_adding(void *val)
-{
-  int SIZE;
-  SIZE = *((int *)val);
-
-  while (1)
-  {
-    sem_wait(&mutex);
-
-    if (counter != SIZE)
-    {
-      total = total + arr[counter];
-
-      printf("Thread2 %d değerini okudu toplam = %d \n", arr[counter], total);
-
-      counter++;
-    }
-    else
-    {
-      printf("Thread2 okunacak değer bulunamadı. \n");
-
-      sem_post(&mutex);
-
-      break;
-    }
-
-    sem_post(&mutex);
-  }
-  return NULL;
-}
-
 void generateRandoms();
+void *thread_1_adding(void *val); // 0 to size
+void *thread_2_adding(void *val); // size to 0
 
 int main()
 {
@@ -112,4 +52,69 @@ void generateRandoms(int lower, int upper, int count)
     int num = (rand() % (upper - lower + 1)) + lower;
     arr[i] = num;
   }
+}
+
+void *thread_1_adding(void *val)
+{
+  int SIZE;
+  int forward = 0;
+  SIZE = *((int *)val);
+
+  while (1)
+  {
+    sem_wait(&mutex);
+
+    if (counter != SIZE)
+    {
+      total = total + arr[forward];
+
+      printf("Thread1 %d değerini okudu toplam = %d \n", arr[forward], total);
+      forward++;
+      counter++;
+    }
+    else
+    {
+      printf("Thread1 okunacak değer bulunamadı.\n");
+
+      sem_post(&mutex);
+
+      break;
+    }
+
+    sem_post(&mutex);
+  }
+  return NULL;
+}
+
+void *thread_2_adding(void *val)
+{
+  int arr_size;
+  int reverse = 1;
+  arr_size = *((int *)val);
+
+  while (1)
+  {
+    sem_wait(&mutex);
+
+    if (counter != arr_size)
+    {
+      total = total + arr[arr_size - reverse];
+
+      printf("Thread2 %d değerini okudu toplam = %d \n", arr[arr_size - reverse], total);
+
+      counter++;
+      reverse++;
+    }
+    else
+    {
+      printf("Thread2 okunacak değer bulunamadı. \n");
+
+      sem_post(&mutex);
+
+      break;
+    }
+
+    sem_post(&mutex);
+  }
+  return NULL;
 }
